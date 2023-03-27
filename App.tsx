c444f604e4ec0ecb3decdd4219cb66418aff13b1
yavor.daskalov <yavor.daskalov.ext@racingpost.com>
2023-03-27 10:36:54 +0300

    added main elements

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import Animated from 'react-native-reanimated';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.screen}>
-      <Text>Makari</Text>
+      <Animated.View style={styles.square} />
+      <TouchableOpacity onPress={() => undefined} style={styles.trigger}>
+        <Text style={styles.triggerText}>TRIGGER</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -14,6 +18,26 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  square: {
+    backgroundColor: 'blue',
+    height: 120,
+    width: 120,
+    borderRadius: 20,
+  },
+  trigger: {
+    position: 'absolute',
+    bottom: 50,
+    height: 40,
+    width: 150,
+    borderRadius: 10,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  triggerText: {
+    color: '#fff',
+    fontWeight: '700',
   },
 });
 
